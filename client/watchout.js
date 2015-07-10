a4129a349bea4fc0fb6f3518139176a678e21f8f
Hack Reactor Students <accounts@hackreactor.com>
2015-07-10 14:56:31 -0700

    Initial commit that sets up global options and our initial SVG canvas for the game.

--- client/watchout.js
+++ client/watchout.js
@@ -1,2 +1,29 @@
 // start slingin' some d3 here.
 
+// global variables
+var score = 0;
+var highScore = 0;
+var collisions = 0;
+
+
+var gameOptions = {
+  height: 450,
+  width: 700,
+  enemyCount: 10,
+  backgroundColor: 'grey'
+};
+
+// initializes our gameboard
+d3.select('.container').append('svg:svg')
+  .attr('width', gameOptions.width)
+  .attr('height', gameOptions.height)
+  .style('background-color', gameOptions.backgroundColor);
+
+
+
+  // gamescore
+  // size
+// classes
+  // balls
+  // player?
+// helper functions
