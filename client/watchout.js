730f1aeca5bfc6c6a744b9863a7c085a8314262e
Hack Reactor Students <accounts@hackreactor.com>
2015-07-10 19:02:12 -0700

    movement!

--- client/watchout.js
+++ client/watchout.js
@@ -10,7 +10,7 @@ var gameOptions = {
   height: 500,
   width: 500,
   enemyCount: 10,
-  bg: 'grey'
+  bg: '#d8d8d8'
 };
 
 // var axiis = {
@@ -20,24 +20,36 @@ var gameOptions = {
 
 
 
-var bodySelection = d3.select(".container");
+var body = d3.select(".container");
 
-var svgSelection = bodySelection.append("svg")
+var svg = body.append("svg")
   .attr("width", gameOptions.width)
   .attr("height", gameOptions.height)
   .style('background-color', gameOptions.bg);
 
 
-
-
-var createCircle = function(){
-  svgSelection.append("circle")
-    .attr("cx", Math.random() * 500)
-    .attr("cy", Math.random() * 500)
+var initiateCircle = function(){
+  svg.append("circle")
+    .attr("cx", Math.random() * 480)
+    .attr("cy", Math.random() * 480)
     .attr("r", 20)
     .style("fill", "red");
+    // transition
+    // duration
 };
 
 for (var i = 0; i < 10; i++) {
-  createCircle();
+  initiateCircle();
 };
+
+var circlesArr = d3.selectAll('circle');
+console.log(circlesArr)
+
+// var goDogGo = function(circle) {
+circlesArr.transition()
+  .duration(1000).attr({
+      cx: Math.random() * 480,
+      cy: Math.random() * 480
+    }); 
+// };
+
