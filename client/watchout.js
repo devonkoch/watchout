c551f0d4e24311dc1f8e77374cc4d49431ff53a0
Hack Reactor Students <accounts@hackreactor.com>
2015-07-11 11:05:32 -0700

    Holy crap! We added enemies and players to our gameboard!

--- client/watchout.js
+++ client/watchout.js
@@ -1,6 +1,6 @@
 // start slingin' some d3 here.
 
-// global variables
+ // global variables
 var score = 0;
 var highScore = 0;
 var collisions = 0;
@@ -36,8 +36,25 @@ for (var i = 0; i < gameOptions.enemyCount; i++) {
 
 var initGameboard = d3.select('.container').append('svg')
                       .attr({
-                        width: 800,
-                        height: 600,
+                        width: 400,
+                        height: 400,
                       })
-                      .style('background-color', '#d8d8d8')
+                      .style('background-color', '#d8d8d8');
+
+var player = d3.select('svg').append('circle')
+              .attr({
+                cx: 200,
+                cy: 200,
+                r: 10,
+                fill: '#ff6600'
+              })
+
+var enemies = d3.select('svg').selectAll('circle').data(gameOptions.enemies, function(d) {return d;})
+                .enter().append('circle')
+                .attr({
+                  cx: function (d) {return Math.random() * 400;},
+                  cy: function (d) {return Math.random() * 400;},
+                  r: 5,
+                  fill: 'black'
+                })
 
